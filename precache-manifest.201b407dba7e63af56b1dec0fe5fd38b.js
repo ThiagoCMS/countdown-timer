fa3c17e4079ecb5f79e1457dbb9d72ec3aacdfc2
ThiagoCMS <thicesar25@gmail.com>
2020-10-01 15:28:58 -0300

    Updates

---
+++ precache-manifest.201b407dba7e63af56b1dec0fe5fd38b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e75c3b23330e3edfe680e622e6e431",
+    "url": "/countdown-timer/index.html"
+  },
+  {
+    "revision": "ba90306231037e5dfbdc",
+    "url": "/countdown-timer/static/js/2.d093cab8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/countdown-timer/static/js/2.d093cab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ab55f2aaa370ab2e2b",
+    "url": "/countdown-timer/static/js/main.112b01db.chunk.js"
+  },
+  {
+    "revision": "e5f6f6931ccaad3b619f",
+    "url": "/countdown-timer/static/js/runtime-main.aefea532.js"
+  }
+]);
